3afc320ea3f05bc759185d89f17cc88c73207e34
leowy <leowyzhuang@gmail.com>
2020-08-06 16:10:20 +0800

    负载均衡组件ribbon

--- ribbon-service/src/main/java/com/ly/springcloud/RibbonServiceApplication.java
+++ ribbon-service/src/main/java/com/ly/springcloud/RibbonServiceApplication.java
@@ -1,10 +1,14 @@
 package com.ly.springcloud;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,9 +28,16 @@ public class RibbonServiceApplication {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
     @GetMapping("/ribbon")
     public String test() {
-        return restTemplate.getForObject("http://EUREKA-CLIENT/test", String.class);
+        // 选择配置NFLoadBalancerRuleClassName中的策略
+        ServiceInstance instance = loadBalancerClient.choose("eureka-client");
+        System.out.println(instance.getUri());
+        // eureka-client屏蔽了eureka，使用本地listOfServers
+        return restTemplate.getForObject("http://eureka-client/test", String.class);
     }
 
     public static void main(String[] args) {
