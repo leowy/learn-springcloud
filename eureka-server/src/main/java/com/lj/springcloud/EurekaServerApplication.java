db19dcecb30a5f60e3e7333c0ff5b2f165e60b55
leowy <leowyzhuang@gmail.com>
2020-08-06 13:35:19 +0800

    注册中心组件eureka

---
+++ eureka-server/src/main/java/com/lj/springcloud/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.lj.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
