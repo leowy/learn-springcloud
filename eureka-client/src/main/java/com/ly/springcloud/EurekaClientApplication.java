6efb5ca50ce7667fdf226596b91d8cdf8b1321d9
leowy <leowyzhuang@gmail.com>
2020-08-06 13:36:53 +0800

    注册中心组件eureka客户端

---
+++ eureka-client/src/main/java/com/ly/springcloud/EurekaClientApplication.java
@@ -0,0 +1,24 @@
+package com.ly.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "hello, spring cloud ~";
+    }
+
+
+}
