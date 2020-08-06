465d4bb67d099418aa4fd0203df6a8d61162c617
leowy <leowyzhuang@gmail.com>
2020-08-06 16:23:18 +0800

    注册中心组件eureka服务端

--- eureka-server/src/main/java/com/lj/springcloud/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/lj/springcloud/EurekaServerApplication.java
@@ -3,7 +3,6 @@ package com.lj.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @EnableEurekaServer
 @SpringBootApplication
